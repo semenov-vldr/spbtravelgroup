252986343547f6248653b62107dfedc57681c70b
Vladimir <vova2066@yandex.ru>
2023-01-03 01:35:18 +0300

    add catalog and transport

---
+++ src/components/transport-service/transport-service.js
@@ -0,0 +1,85 @@
+{
+
+  let mySwiper;
+
+  const swiperList = document.querySelectorAll('.transport-service__content');
+
+  if (swiperList) createSwiper(swiperList);
+
+
+
+  function createSwiper (swiperList) {
+    swiperList.forEach(swiper => {
+
+      mySwiper = new Swiper(swiper, {
+        // pagination: {
+        //   el: '.swiper-pagination',
+        //   clickable: true,
+        // },
+        // navigation: {
+        //   nextEl: '.slider-nav__next',
+        //   prevEl: '.slider-nav__prev',
+        // },
+
+        // scrollbar: {
+        //   el: '.swiper-scrollbar',
+        //   draggable: true,
+        // },
+
+        uniqueNavElements: true,
+
+        slideClass: 'transport-service__item',
+
+        //slidesPerView: 4,
+
+        // Бесконечная прокрутка
+        //loop: true,
+
+        // Откл функционала, если слайдов меньше, чем нужно
+        watchOverflow: true,
+
+        //centeredSlides: true,
+
+        // Отступ между слайдами
+        spaceBetween: 24,
+
+        // Стартовый слайд
+        initialSlide: 0,
+
+        // Брейк поинты (адаптив)
+        // Ширина экрана
+        breakpoints: {
+          320: {
+            slidesPerView: 1.1,
+            spaceBetween: 16,
+          },
+
+          480: {
+            slidesPerView: 2.2,
+            spaceBetween: 20,
+          },
+
+          768: {
+            slidesPerView: 3.2
+          },
+
+          1100: {
+            slidesPerView: 4
+          },
+
+        }
+      });
+
+    })
+  };
+
+
+
+
+
+
+
+
+
+
+}
