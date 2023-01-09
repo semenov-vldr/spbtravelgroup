c4e417d2985999ac3e26cb16ced650126053f543
semenov-vldr <semenov-vldr@users.noreply.github.com>
2023-01-09 20:39:12 +0000

    deploy: 0aa3978ab81af626daca9215ee72ba02ae218c8b

--- assets/js/main.js
+++ assets/js/main.js
@@ -127,6 +127,27 @@ if (main) {
 }
 
 
+const envlopeWrapper = document.querySelector('.envlope-wrapper');
+
+const envelope = envlopeWrapper.querySelector('#envelope');
+const btnOpen = document.querySelector('#open');
+const btnReset = document.querySelector('#reset');
+
+
+envelope.addEventListener('click', open)
+// btnOpen.addEventListener('click', open)
+// btnReset.addEventListener('click', close)
+
+function open () {
+  envelope.classList.remove('close');
+  envelope.classList.add('open')
+}
+
+function close () {
+  envelope.classList.remove('open')
+  envelope.classList.add('close');
+}
+
 {
 
   let mySwiper;
