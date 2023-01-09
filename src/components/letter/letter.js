0aa3978ab81af626daca9215ee72ba02ae218c8b
Vladimir <vova2066@yandex.ru>
2023-01-09 23:38:29 +0300

    add letter

---
+++ src/components/letter/letter.js
@@ -0,0 +1,20 @@
+const envlopeWrapper = document.querySelector('.envlope-wrapper');
+
+const envelope = envlopeWrapper.querySelector('#envelope');
+const btnOpen = document.querySelector('#open');
+const btnReset = document.querySelector('#reset');
+
+
+envelope.addEventListener('click', open)
+// btnOpen.addEventListener('click', open)
+// btnReset.addEventListener('click', close)
+
+function open () {
+  envelope.classList.remove('close');
+  envelope.classList.add('open')
+}
+
+function close () {
+  envelope.classList.remove('open')
+  envelope.classList.add('close');
+}
