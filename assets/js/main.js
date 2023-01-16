6ac645d12ec66f5ca16346a4398b109aad0b40b4
semenov-vldr <semenov-vldr@users.noreply.github.com>
2023-01-16 20:26:28 +0000

    deploy: 315e0842718e05ad143a59ca8685315af0c1bfc4

--- assets/js/main.js
+++ assets/js/main.js
@@ -55,6 +55,42 @@ new Swiper('.excursion__slider-wrapper', {
 });
 
 
+// Показать больше
+const filter = document.querySelector('.filter');
+
+const widthMobile = window.matchMedia('(max-width: 768px)').matches;
+
+if (filter) {
+  const showMore = filter.querySelector('.filter__tabs-item--more');
+
+  let startItems;
+
+  widthMobile ? startItems = 6 : startItems = 3;
+
+  const filterItems = filter.querySelectorAll('.filter__tabs-item');
+  const filterItemsLength = filterItems.length;
+
+  showMore.addEventListener('click', () => {
+    filterItems.forEach(el => el.classList.add('js-visible'));
+    showMore.classList.add('hidden');
+  })
+
+  if (filterItemsLength < startItems + 1) showMore.classList.add('hidden');
+
+
+  filterItems.forEach(filterItem => {
+    filterItem.addEventListener('click', () => {
+
+      filterItems.forEach(item => item.classList.remove('js-filter-tabs-active'))
+
+      filterItem.classList.add('js-filter-tabs-active');
+    })
+  })
+
+}
+
+
+
 const footer = document.querySelector('.footer');
 const copyrightYear = footer.querySelector('.footer__copyright span');
 copyrightYear.textContent = new Date().getFullYear();
@@ -275,6 +311,7 @@ if (envlopeWrapper) {
 
 
 }
+
 
 
 const html = document.querySelector('html');
