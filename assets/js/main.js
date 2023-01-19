9b76c23d485d9508e12bcc571792f98e3e390b2a
semenov-vldr <semenov-vldr@users.noreply.github.com>
2023-01-19 15:18:04 +0000

    deploy: 13b74cff4f7b2dfcf0d4d7559b05fa1eceffaddf

--- assets/js/main.js
+++ assets/js/main.js
@@ -390,6 +390,21 @@ if (envlopeWrapper) {
 }
 
 
+const API_URL = "./assets/json/allExcursionCards.json";
+
+function getExcursionCards () {
+  return fetch(API_URL)
+    .then(responce => responce.json())
+    .then(cards => cards)
+    .catch(() => console.log('Ошибка при загрузке данных'));
+};
+
+
+
+getExcursionCards().then(cards => {
+  cards.forEach(card => createExcursionCard(card))
+})
+
 
 const html = document.querySelector('html');
 
@@ -410,6 +425,64 @@ function unblockScrollBody () {
 
 function toggleScrollBody () {
   html.classList.toggle(classBlockScroll);
+};
+
+const cardTemplate = document.querySelector('#card').content.querySelector('.excursion-card');
+const excursionGrid = document.querySelector('.excursion__grid'); // блок для вставки карточек
+const photoListElement = document.querySelector('.excursion-card__image');
+const photoListElementFragment = document.createDocumentFragment();
+
+
+
+function createExcursionCard (item) {
+
+  const cardItem = cardTemplate.cloneNode(true);
+
+
+
+  function isImage(src) {
+    let resultCheck = true;
+    let image = cardItem.querySelector('.excursion-card__image img');
+    image.src = src;
+    image.onerror = function () {
+      resultCheck = false;
+    };
+    return resultCheck;
+  };
+
+
+  console.log(isImage(item.srcImg))
+
+
+
+
+  if (!item.srcImg) {
+    cardItem.querySelector('.excursion-card__image img').style.zIndex = '-1';
+    cardItem.querySelector('.excursion-card__image').style.backgroundColor = 'antiquewhite';
+  }
+
+
+  cardItem.querySelector('.excursion-card__image img').src = item.srcImg;                   // Картинка
+  cardItem.querySelector('.excursion-card__badge').textContent = item.duration;             // Продолжительность экскурсии
+  cardItem.querySelector('.excursion-card__title').textContent = item.title;                // Заголовок
+  cardItem.querySelector('.excursion-card__timetable-days').textContent = `${item.days}:`;  // Дни недели проведения экскурсии
+  cardItem.querySelector('.excursion-card__timetable-time').textContent = item.time;        // Время проведения экскурсии
+  cardItem.querySelector('.excursion-card__price span').textContent = `${item.price}₽`;     // Стоимость экскурсии
+  cardItem.querySelector('.excursion-card__price a.button').href = item.link;               // Ссылка на экскурсию
+
+  // Добавление пунктов описания (с галочкой)
+  const descriptionList = cardItem.querySelector('.excursion-card__description-list');
+  item.descriptionList.forEach(descriptionItem => {
+    const li = document.createElement('li');
+    li.classList.add('excursion-card__description-item');
+    li.textContent = descriptionItem;
+    descriptionList.appendChild(li);
+  });
+
+  excursionGrid.appendChild(cardItem);
+
+  return cardItem;
+
 };
 
 fetch("./assets/json/data.json")
