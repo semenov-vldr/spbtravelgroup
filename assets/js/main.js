2695548a4b66a11719c4103d40dc4cb4929d374b
semenov-vldr <semenov-vldr@users.noreply.github.com>
2023-01-20 15:56:18 +0000

    deploy: 38fc378efd6122032570f6356e4a492cb15a96f8

--- assets/js/main.js
+++ assets/js/main.js
@@ -404,6 +404,38 @@ if (envlopeWrapper) {
 
 }
 
+const gotTopButton = document.querySelector('.go-top');
+
+if (gotTopButton) {
+
+  gotTopButton.addEventListener('click', goTop);
+
+  function goTop () {
+    window.scrollTo({
+      top: 0,
+      left: 0,
+      behavior: 'smooth'
+    })
+  };
+
+
+
+  window.addEventListener('scroll', trackScroll);
+
+  function trackScroll () {
+    const offset = window.scrollY;
+
+    const coords = document.documentElement.clientHeight;
+
+    if (offset > coords) {
+      gotTopButton.classList.add('js-scroll-show');
+    } else {
+      gotTopButton.classList.remove('js-scroll-show');
+    }
+  }
+
+}
+
 
 const API_URL = "./assets/json/allExcursionCards.json";
 
