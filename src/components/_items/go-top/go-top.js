38fc378efd6122032570f6356e4a492cb15a96f8
Vladimir <vova2066@yandex.ru>
2023-01-20 18:55:42 +0300

    add go-top btn

---
+++ src/components/_items/go-top/go-top.js
@@ -0,0 +1,31 @@
+const gotTopButton = document.querySelector('.go-top');
+
+if (gotTopButton) {
+
+  gotTopButton.addEventListener('click', goTop);
+
+  function goTop () {
+    window.scrollTo({
+      top: 0,
+      left: 0,
+      behavior: 'smooth'
+    })
+  };
+
+
+
+  window.addEventListener('scroll', trackScroll);
+
+  function trackScroll () {
+    const offset = window.scrollY;
+
+    const coords = document.documentElement.clientHeight;
+
+    if (offset > coords) {
+      gotTopButton.classList.add('js-scroll-show');
+    } else {
+      gotTopButton.classList.remove('js-scroll-show');
+    }
+  }
+
+}
